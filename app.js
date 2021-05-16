260e14482a28c8cd45e26147a780fdb52b2da2f4
barron <barron.van.deusen@gmail.com>
2021-05-16 16:05:05 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Lullabies to Paralyze'));
+app.get('/', (req, res) => res.send('The Way You Used To Do'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
